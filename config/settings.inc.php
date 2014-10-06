05387344b683e5c583523fb80cb85923704f0391
marcelo.teixeira <contacto.sexoja@gmail.com>
2014-10-06 13:54:56 +0100

    aasd

---
+++ config/settings.inc.php
@@ -0,0 +1,19 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'lucy');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', '37zGWCgn7BfcnAhdTIG9Rj2KUJcu5GRp0N8Cwnr1rNYGWpGnNnYSc7Dh');
+define('_COOKIE_IV_', '0JI2fXDx');
+define('_PS_CREATION_DATE_', '2014-10-06');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.9');
+define('_RIJNDAEL_KEY_', 'bGgMty788fNgOMEuEQn5hEhBbHbxrshf');
+define('_RIJNDAEL_IV_', 'IZk3b6C1qsNNVenxWlivOQ==');
